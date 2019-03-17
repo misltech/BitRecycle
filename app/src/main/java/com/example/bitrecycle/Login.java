1bafda65376abae4b6ad5298fa5925fc82b4731e
Tahir <tahir.mitchell12@guttmanmail.cuny.edu>
2019-03-17 00:51:01 -0400

    idk at this point

--- app/src/main/java/com/example/bitrecycle/Login.java
+++ app/src/main/java/com/example/bitrecycle/Login.java
@@ -2,12 +2,28 @@ package com.example.bitrecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.*;
+import android.view.View;
+import android.content.Intent;
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ImageView initSnapLogin = findViewById(R.id.snaplogin_logo);
+
+
+        initSnapLogin.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            public void onClick(View view) {
+
+                System.out.println("Started");
+                //startActivity(new Intent(Login.this, MainActivity.class));
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/bitrecycle/MainActivity.java
+++ app/src/main/java/com/example/bitrecycle/MainActivity.java
@@ -38,11 +38,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //mTextMessage = (TextView) findViewById(R.id.message);
+        //BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 }
